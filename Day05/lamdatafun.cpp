a36daf471b0cccddd012382b2a1c59f1367b701e
KUNDAN5678 <kundankumar5678k.s@gmail.com>
2026-08-07 17:26:05 +0000

    Day05 funtion lamda class

---
+++ Day05/lamdatafun.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <functional> // Needed for std::function
+using namespace std;
+
+
+void myFunction(function<void()> func) {
+  func();
+  func();
+}
+
+int main(){
+    auto message = []() {
+    cout << "Hello World!\n";
+  };
+
+  myFunction(message);
+  return 0;
+
+}
+
+
